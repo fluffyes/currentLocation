ed4eb6361b6053dea68688dd1f1692ab0ffab2c0
Axel <soulchildsince2013@gmail.com>
2019-01-25 01:02:25 +0800

    Initial Commit

---
+++ currentLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  currentLocation
+//
+//  Created by Soulchild on 25/01/2019.
+//  Copyright © 2019 fluffy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
