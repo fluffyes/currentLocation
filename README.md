5ea3f6f50a2881649ea1a8928e4d1c2fffdb4dd5
Axel <soulchildsince2013@gmail.com>
2019-01-29 16:15:59 +0800

    Added project

---
+++ README.md
@@ -0,0 +1,3 @@
+# currentLocation
+
+Sample iOS Project to demonstrate basic usage of CoreLocation to retrieve current location data of user
--- currentLocation/ViewController.swift
+++ currentLocation/ViewController.swift
@@ -7,14 +7,97 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        
+        self.activityIndicator.isHidden = true
     }
 
 
+    @IBAction func getCurrentLocationTapped(_ sender: Any) {
+        retriveCurrentLocation()
+    }
+    
+    func retriveCurrentLocation(){
+        let status = CLLocationManager.authorizationStatus()
+        
+        if(status == .denied || status == .restricted || !CLLocationManager.locationServicesEnabled()){
+            return
+        }
+        
+        // if haven't show location permission dialog before, show it to user
+        if(status == .notDetermined){
+            locationManager.requestWhenInUseAuthorization()
+            
+            // if you want the app to retrieve location data even in background, use requestAlwaysAuthorization
+            //locationManager.requestAlwaysAuthorization()
+            return
+        }
+        
+        self.activityIndicator.isHidden = false
+        self.activityIndicator.startAnimating()
+        
+        // request location data for one-off usage
+        locationManager.requestLocation()
+        
+        // keep requesting location data until stopUpdatingLocation() is called
+        // locationManager.startUpdatingLocation()
+    }
 }
 
+extension ViewController : CLLocationManagerDelegate {
+    // called when the authorization status is changed for the core location permission
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("location manager authorization status changed")
+        
+        switch status {
+        case .authorizedAlways:
+            print("user allow app to get location data when app is active or in background")
+        case .authorizedWhenInUse:
+            print("user allow app to get location data only when app is active")
+        case .denied:
+            print("user tap 'disallow' on the permission dialog, cant get location data")
+        case .restricted:
+            print("parental control setting disallow location data")
+        case .notDetermined:
+            print("the location permission dialog haven't shown before, user haven't tap allow/disallow")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // the last element is the most recent location
+        if let location = locations.last {
+            self.latitudeLabel.text = "\(location.coordinate.latitude)"
+            self.longitudeLabel.text = "\(location.coordinate.longitude)"
+        }
+        
+        for location in locations {
+            // do stuff with each location
+        }
+        
+        self.activityIndicator.stopAnimating()
+        self.activityIndicator.isHidden = true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+        self.activityIndicator.stopAnimating()
+        self.activityIndicator.isHidden = true
+    }
+    
+}
